d19461d721c78cc6a9d5695d12f4c5774c817f46
pranavikadatla <142671406+pranavikadatla@users.noreply.github.com>
2024-07-09 16:54:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -39,7 +39,7 @@ function rot13(encodedStr) {
 		decodedArr.push(lookup[char] || char);
 	}
 
-  return decodedArr;
+  return decodedArr.join("");
 }
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
